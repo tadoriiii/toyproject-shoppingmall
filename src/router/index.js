1819452c3828d711d87722a8b67de7fbef99fabc
tadoriiii <tw772@naver.com>
2021-09-29 01:31:44 +0900

    start project

---
+++ src/router/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+
+const router = () => {
+  return <BrowserRouter></BrowserRouter>;
+};
+
+export default router;
